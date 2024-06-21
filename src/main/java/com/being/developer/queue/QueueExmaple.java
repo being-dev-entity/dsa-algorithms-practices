8ce633612c0750966a4417968f2a82dedac24c7b
Abhay Pratap Singh <pratap.abhay98@gmail.com>
2024-06-21 19:06:38 +0000

    add priority queue

--- src/main/java/com/being/developer/queue/QueueExmaple.java
+++ src/main/java/com/being/developer/queue/QueueExmaple.java
@@ -1,7 +1,9 @@
 package com.being.developer.queue;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class QueueExmaple {
     public static void main(String[] args) {
@@ -34,11 +36,11 @@ public class QueueExmaple {
 
     public static void queueOperations() {
         PriorityQueue<String> queue = new PriorityQueue<>();
-        queue.add("Abhay");
-        queue.add("Rahul");
-        queue.add("Rohit");
-        queue.add("Sachin");
-        queue.add("Virat");
+        queue.offer("Abhay");
+        queue.offer("Rahul");
+        queue.offer("Rohit");
+        queue.offer("Sachin");
+        queue.offer("Virat");
         System.out.println("Queue: " + queue);
         System.out.println("Head: " + queue.peek());
         System.out.println("Element: " + queue.element());
@@ -49,8 +51,8 @@ public class QueueExmaple {
             System.out.println("Queue: " + queue);
         }
         // example quque will sorted alpahebatically.
-        queue.add("abc1");
-        queue.add("aaa");
+        queue.offer("abc1");
+        queue.offer("aaa");
         System.out.println("Queue: " + queue);
         System.out.println("Head: " + queue.peek());
 
@@ -64,28 +66,28 @@ public class QueueExmaple {
 
     public static void queuePriority() {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(15);
-        queue.add(50);
-        queue.add(25);
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(15);
+        queue.offer(50);
+        queue.offer(25);
         System.out.println("Queue: " + queue);
         // Queue: [10, 20, 15, 50, 25]
         PriorityQueue<Integer> queue2 = new PriorityQueue<>(Collections.reverseOrder());
-        queue2.add(10);
-        queue2.add(20);
-        queue2.add(15);
-        queue2.add(50);
-        queue2.add(25);
+        queue2.offer(10);
+        queue2.offer(20);
+        queue2.offer(15);
+        queue2.offer(50);
+        queue2.offer(25);
         System.out.println("Queue2: " + queue2);
         // Queue2: [50, 25, 15, 10, 20]
         PriorityQueue<String> queue3 = new PriorityQueue<>();
-        queue3.add("abc");
-        queue3.add("aaa");
-        queue3.add("ccc");
-        queue3.add("bbb");
-        queue3.add("cab");
-        queue3.add("bca");
+        queue3.offer("abc");
+        queue3.offer("aaa");
+        queue3.offer("ccc");
+        queue3.offer("bbb");
+        queue3.offer("cab");
+        queue3.offer("bca");
         System.out.println("Queue3: " + queue3);
         // Queue3: [aaa, abc, bca, bbb, cab, ccc]
 
@@ -96,6 +98,25 @@ public class QueueExmaple {
         queue4.offer(50);
         queue4.offer(25);
         System.out.println("Queue4: " + queue4);
+
+
+        Queue<String> queue5 = new LinkedList<>();
+        queue5.offer("A");
+        queue5.offer("C");
+        queue5.offer("B");
+        queue5.offer("F");
+        queue5.offer("D");
+        System.out.println("Queue5: " + queue5);
+        // Queue5: [A, C, B, F, D]
+
+        Queue<String> queue6 = new PriorityQueue<>();
+        queue6.offer("A");
+        queue6.offer("C");
+        queue6.offer("B");
+        queue6.offer("F");
+        queue6.offer("D");
+        System.out.println("Queue6: " + queue6);
+        // Queue6: [A, C, B, F, D]
     }
 
 }
