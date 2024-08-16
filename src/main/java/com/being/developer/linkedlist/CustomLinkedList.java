4196ce90a6c37a76d68d8455a087d337c770215d
Abhay Pratap Singh <pratap.abhay98@gmail.com>
2024-08-16 23:42:27 +0530

    add basic custom linked list

--- src/main/java/com/being/developer/linkedlist/CustomLinkedList.java
+++ src/main/java/com/being/developer/linkedlist/CustomLinkedList.java
@@ -1,14 +1,73 @@
 package com.being.developer.linkedlist;
 
+import java.util.Objects;
+
 public class CustomLinkedList {
-     
-   
+    private Node head;
+    private Node tail;
+    private int size;
 
+    public CustomLinkedList() {
+    }
 
+    public void add(int value) {
+
+        if (Objects.isNull(head)) {
+            head = new Node(value);
+            tail = head;
+            size++;
+            return;
+        }
+        tail.next = new Node(value);
+        tail = tail.next;
+        size++;
+    }
+
+    public void remove(int value) {
+        if (head.value == value) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        Node prevNode = null;
+
+        while (Objects.nonNull(current)) {
+            if (current.value == value) {
+                prevNode.next = current.next;
+                // tail element is being removed then prevNode will be tail now.
+                if (Objects.isNull(current.next)) {
+                    tail = prevNode;
+                }
+                size--;
+                break;
+            }
+            prevNode = current;
+            current = current.next;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void traverse() {
+        Node current = head;
+        while (Objects.nonNull(current)) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
 }
 
 class Node {
     public int value;
-    public Node head;
     public Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/being/developer/linkedlist/LinkedListExample.java
+++ src/main/java/com/being/developer/linkedlist/LinkedListExample.java
@@ -2,8 +2,25 @@ package com.being.developer.linkedlist;
 
 public class LinkedListExample {
     public static void main(String[] args) {
-        
+        System.out.println("Linked List examples!");
+
+        CustomLinkedList customLinkedList = new CustomLinkedList();
+        customLinkedList.add(10);
+        customLinkedList.add(20);
+        customLinkedList.add(30);
+        customLinkedList.add(40);
+        customLinkedList.add(50);
+
+        System.out.println(customLinkedList.isEmpty());
+        System.out.println(customLinkedList.size());
+
+        customLinkedList.traverse();
+        customLinkedList.remove(50); // 10
+        System.out.println(customLinkedList.size());
+        customLinkedList.traverse();
+
+        customLinkedList.add(70);
+        System.out.println(customLinkedList.size());
+        customLinkedList.traverse();
     }
 }
-
-
