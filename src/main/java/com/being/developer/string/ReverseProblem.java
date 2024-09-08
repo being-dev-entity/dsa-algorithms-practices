857a1adaf983ec85fcadf2329ba525b658585cec
Abhay Pratap Singh <pratap.abhay98@gmail.com>
2024-09-08 13:47:54 +0530

    reverse string example

---
+++ src/main/java/com/being/developer/string/ReverseProblem.java
@@ -0,0 +1,34 @@
+package com.being.developer.string;
+
+public class ReverseProblem {
+    public static void main(String[] args) {
+        System.out.println("String Reverse Example!");
+        String input = "Ramayan";
+        String output = reverseOptimized(input);
+        System.out.println(input + " Reverse: " + output);
+
+    }
+
+    private static String reverse(String input) {
+        String output = null;
+        char[] chars = new char[input.length()];
+        for (int i = input.length() - 1; i >= 0; i--) {
+            chars[input.length() - 1 - i] = input.charAt(i);
+        }
+        output = new String(chars);
+        return output;
+    }
+
+    private static String reverseOptimized(String input) {
+        String output = null;
+        char[] chars = new char[input.length()];
+        int mid = input.length() / 2; // 3
+        for (int i = mid; i >= 0; i--) {
+            chars[input.length() - 1 - i] = input.charAt(i); // 3&3, 4&2, 5&1, 6&0;
+            chars[i] = input.charAt(input.length() - 1 - i); // 3&3, 2&4, 1&5, 0&6;
+        }
+        output = new String(chars);
+        return output;
+    }
+
+}
