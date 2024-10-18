91dbd161d2fe707205e98e4ebf8829c71eb2a1a7
Abhay Pratap Singh <pratap.abhay98@gmail.com>
2024-10-18 23:44:27 +0530

    fixed indent

--- src/main/java/com/being/developer/programs/FactorialExample.java
+++ src/main/java/com/being/developer/programs/FactorialExample.java
@@ -1,44 +1,44 @@
 package com.being.developer.programs;
 
 public class FactorialExample {
-  public static void main(String arg[]) {
-    System.out.println("Factorial Example!");
-    long factorial5 = findFactorial(5);
-    long factorial2 = findFactorialRec(2);
-    System.out.println(factorial5);
-    System.out.println(factorial2);
-    int numberOfTrailingZero = findTrailingZeros(5);
-    System.out.println("numberOfTrailingZero: " + numberOfTrailingZero);
-  }
-
-  private static long findFactorial(int number) {
-    long factorial = 1;
-    if (number == 0) {
-      return 1;
-    }
-    int i = 0;
-    while (number > 0) {
-      factorial = factorial * number;
-      number--;
-    }
-
-    return factorial;
-  }
-
-  private static long findFactorialRec(int number) {
-    if (number == 0) {
-      return 1;
-    }
-    return number * findFactorialRec(number - 1);
-  }
-
-  private static int findTrailingZeros(int number) {
-    int res = 0;
-
-    for (int i = 5; i <= number; i = i * 5) {
-      res = res + res + number / 5;
+    public static void main(String arg[]) {
+        System.out.println("Factorial Example!");
+        long factorial5 = findFactorial(5);
+        long factorial2 = findFactorialRec(2);
+        System.out.println(factorial5);
+        System.out.println(factorial2);
+        int numberOfTrailingZero = findTrailingZeros(5);
+        System.out.println("numberOfTrailingZero: " + numberOfTrailingZero);
     }
     
-    return res;
-  }
+    private static long findFactorial(int number) {
+        long factorial = 1;
+        if (number == 0) {
+            return 1;
+        }
+        int i = 0;
+        while (number > 0) {
+            factorial = factorial * number;
+            number--;
+        }
+        
+        return factorial;
+    }
+    
+    private static long findFactorialRec(int number) {
+        if (number == 0) {
+            return 1;
+        }
+        return number * findFactorialRec(number - 1);
+    }
+    
+    private static int findTrailingZeros(int number) {
+        int res = 0;
+        
+        for (int i = 5; i <= number; i = i * 5) {
+            res = res + res + number / 5;
+        }
+        
+        return res;
+    }
 }
