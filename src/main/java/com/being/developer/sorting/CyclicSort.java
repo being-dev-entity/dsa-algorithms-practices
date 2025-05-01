175c94548c2f927b3df2369ee28153cce1df4a08
“being-dev-entity” <pratap.abhay98@gmail.com>
2025-05-01 22:30:50 +0530

    add cyclic sort

---
+++ src/main/java/com/being/developer/sorting/CyclicSort.java
@@ -0,0 +1,39 @@
+package com.being.developer.sorting;
+
+// Cyclic sort works when all number ranges in the array lengh 1 to n where n is length;
+public class CyclicSort {
+    public static void main(String[] args) {
+        System.out.println("Cylic sort example..");
+        int array[] = { 2, 1, 3, 6, 4, 5 };
+
+        for (int num : array) {
+            System.out.print(num + ", ");
+        }
+
+        sort(array);
+
+        System.out.println("\nAfter cyclic sort");
+        for (int num : array) {
+            System.out.print(num + ", ");
+        }
+
+    }
+
+    public static void sort(int[] array) {
+        int i = 0;
+        while (i < array.length) {
+            int currectIndex = array[i] - 1;
+            if (array[i] != array[currectIndex]) {
+                swap(array, i, currectIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int array[], int i, int currectIndex) {
+        int temp = array[i];
+        array[i] = array[currectIndex];
+        array[currectIndex] = temp;
+    }
+}
